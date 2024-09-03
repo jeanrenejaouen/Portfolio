7bf12501b7981866238f3f059a3ff0013dbfcddb
jeanrenejaouen <jean-rene.jaouen@wanadoo.fr>
2024-09-03 19:23:15 +0200

    Commenter erreur due a la version de React

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import Router from './components/router';
 import './_index.scss'
 
 
+/* CORRIGER ERREUR : react-dom.development.js:86 Warning: ReactDOM.render is no longer supported in React 18. 
+Use createRoot instead. Until you switch to the new API, your app will behave as if it's running React 17. 
+Learn more: https://reactjs.org/link/switch-to-createroot/* 
 
-/* ReactDOM.render(
+ReactDOM.render(
   <React.StrictMode>
     <Router />
   </React.StrictMode>,
   document.getElementById('root')
 ) */
+
 ReactDOM.createRoot(document.querySelector("#root")).render(<React.StrictMode><Router /></React.StrictMode>);
